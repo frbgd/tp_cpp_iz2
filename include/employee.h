78b5fba7dcb65b610f02fa82f817179982e67e55
frbgd <kap17597@yandex.ru>
2021-03-27 15:26:58 +0300

    employee struct

---
+++ include/employee.h
@@ -0,0 +1,18 @@
+#ifndef TP_CPP_IZ2_EMPLOYEE_H
+#define TP_CPP_IZ2_EMPLOYEE_H
+
+#define POSITION_STR_LEN 16
+#define SECOND_NAME_STR_LEN 23
+#define FIRST_NAME_STR_LEN 16
+
+struct employee {
+    char position[POSITION_STR_LEN];        // 16 байт
+    unsigned short age;                     // 2 байта
+    unsigned short work_experience;         // 2 байта
+    unsigned salary;                        // 4 байта
+    char second_name[SECOND_NAME_STR_LEN];  // 23 байта
+    _Bool is_male;                          // 1 байт
+    char first_name[FIRST_NAME_STR_LEN];    // 16 байт
+};                                          // всего 64 байта
+
+#endif //TP_CPP_IZ2_EMPLOYEE_H
