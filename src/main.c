c9d9899999de89ce830aac06c454720738d7ec06
frbgd <kap17597@yandex.ru>
2021-03-28 22:15:08 +0300

    free array

--- src/main.c
+++ src/main.c
@@ -1,3 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/employee.h"
+
 int main() {
+    char *file_name = "test_data.bin";
+    int employees_number = read_employees_number_from_file(file_name);
+    if (employees_number < 0) {
+        return -1;
+    }
+    array employee_list;
+    init_array(&employee_list, employees_number);
+    if (read_employees_from_file(file_name, employees_number, &employee_list) != 0) {
+        return -1;
+    }
+
+
+
+    free_array(&employee_list);
     return 0;
 }
