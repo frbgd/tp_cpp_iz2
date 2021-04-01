43fd5d9809916f77598c6a9c8d5f84075ba39da6
frbgd <44981068+frbgd@users.noreply.github.com>
2021-04-01 11:27:49 +0300

    gtests template

---
+++ tests/tests.cpp
@@ -0,0 +1,38 @@
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "../include/employee.h"
+}
+
+TEST(employees, FirstPositionMinAgeEmployee) {
+
+}
+
+TEST(employees, FirstPositionMaxAgeEmployee) {
+
+}
+
+TEST(employees, SecondPositionMinAgeEmployee) {
+
+}
+
+TEST(employees, SecondPositionMaxAgeEmployee) {
+
+}
+
+TEST(employees, ThirdPositionMinAgeEmployee) {
+
+}
+
+TEST(employees, ThirdPositionMaxAgeEmployee) {
+
+}
+
+TEST(employees, CorrectSecondNamesSort) {
+
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
